87b369db190b1d4588974251651124576e529984
Begench <begenchgeldyev@gmail.com>
2024-07-30 01:40:37 +0700

    test: hero image

--- src/app.tsx
+++ src/app.tsx
@@ -5,19 +5,23 @@ import styles from './app.module.sass'
 import { ProjectsSection } from './home/projects-section/projects-section'
 import { HorizontalScroll } from 'components/horizontal-scroll/horizontal-scroll'
 import { WhyWeSection } from './home/why-we-section/why-we-section'
+import Logo from './images/logo.svg'
 
 export const App = () => {
 	return (
 		<section className={styles.main}>
 			<div className='content_grid'>
 				<Header />
-				<ProjectsSection />
-				<div className='full_width'>
+				<div className={styles.hero}>
+					<Logo className={styles.logo} />
+				</div>
+				{/* <ProjectsSection /> */}
+				{/* <div className='full_width'>
 					<HorizontalScroll />
 				</div>
 				<div className='breakout'>
 					<WhyWeSection />
-				</div>
+				</div> */}
 			</div>
 		</section>
 	)
